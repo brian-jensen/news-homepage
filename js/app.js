30a7648a88c7ed02e75db4fc96dfdd153440361a
Brian Jensen <31598338+brian-jensen@users.noreply.github.com>
2022-10-27 04:09:15 -0700

    Add hover states & final JS

--- js/app.js
+++ js/app.js
@@ -5,6 +5,17 @@ const overlay = document.querySelector('.overlay');
 hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
     navLinks.classList.toggle('active');
-    overlay.classList.toggle('active'); 
+    overlay.classList.toggle('active');
+    if (navLinks.classList.contains('active')) {
+      navLinks.addEventListener('click', e => {
+        if (e.target.closest('a')) {
+          hamMenu.classList.remove('active');
+          navLinks.classList.remove('active');
+          overlay.classList.remove('active');
+        }
+      });
+    }
 });
+
+
 
